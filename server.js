b7fc3faadf503da2312855b6909f1f8dcea524c6
Muneeb Ali <muneebbwp@gmail.com>
2021-01-23 14:56:30 -0600

    nothing much

--- server.js
+++ server.js
@@ -4,15 +4,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 
 
-mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/workout',
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
-  }
-);
+
 
 const PORT = process.env.PORT || 3000;
 
@@ -25,7 +17,15 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/workout',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
 const db = require("./models");
 
