aed52168584f32987f9d02a08c8a735f657b08ad
Muneeb Ali <muneebbwp@gmail.com>
2021-01-23 11:51:13 -0600

    making some schanges

---
+++ server.js
@@ -0,0 +1,33 @@
+
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/workout',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+
+);
+
+const app = express();
+
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+
+
+const db = require("./models");
+
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
