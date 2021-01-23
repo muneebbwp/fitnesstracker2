a371175ec36ac479290d80f6bed2f1b37a9cb205
Muneeb Ali <muneebbwp@gmail.com>
2021-01-23 12:37:13 -0600

    updated server.js

--- server.js
+++ server.js
@@ -5,6 +5,16 @@ const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
 
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
+
 const app = express();
 
 app.use(logger("dev"));
