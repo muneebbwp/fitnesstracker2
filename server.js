9f8f769cf334185251aa9658959d1580fff517e6
Muneeb Ali <muneebbwp@gmail.com>
2021-01-26 17:08:42 -0600

    fixing issues with heroku

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Connection with the database
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitnessTracker", {
     useNewUrlParser: true,
     useFindAndModify: false,
     useUnifiedTopology: true,
